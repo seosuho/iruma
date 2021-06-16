258946f3180c2153a10f561d68ca36f1d3bcd7dc
sean <sean@kakaoenterprise.com>
2021-06-16 16:04:12 +0900

    init project

---
+++ src/main/kotlin/com/kep/iruma/IrumaApplication.kt
@@ -0,0 +1,11 @@
+package com.kep.iruma
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IrumaApplication
+
+fun main(args: Array<String>) {
+    runApplication<IrumaApplication>(*args)
+}
---
+++ src/test/kotlin/com/kep/iruma/IrumaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kep.iruma
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IrumaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
